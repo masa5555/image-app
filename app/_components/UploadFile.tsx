89c01ad83e5a4379d901559b8025396a375bc3a2
masa5555 <masa57887@gmail.com>
2024-09-05 11:06:59 +0900

    Add: 簡素なUI

---
+++ app/_components/UploadFile.tsx
@@ -0,0 +1,43 @@
+'use client';
+
+import { type PutBlobResult } from '@vercel/blob';
+import { upload } from '@vercel/blob/client';
+import { useState, useRef } from 'react';
+
+export function UploadFile() {
+  const inputFileRef = useRef<HTMLInputElement>(null);
+  const [blob, setBlob] = useState<PutBlobResult | null>(null);
+  return (
+    <>
+      <h1>Upload Your Image</h1>
+
+      <form
+        onSubmit={async (event) => {
+          event.preventDefault();
+
+          if (!inputFileRef.current?.files) {
+            throw new Error('No file selected');
+          }
+
+          const file = inputFileRef.current.files[0];
+
+          const newBlob = await upload(file.name, file, {
+            access: 'public',
+            handleUploadUrl: '/images',
+          });
+          console.log({newBlob})
+
+          setBlob(newBlob);
+        }}
+      >
+        <input name="file" ref={inputFileRef} type="file" required />
+        <button type="submit" className="bg-white shadow p-1">Upload</button>
+      </form>
+      {blob && (
+        <div>
+          Blob url: <a href={blob.url}>{blob.url}</a>
+        </div>
+      )}
+    </>
+  );
+}
